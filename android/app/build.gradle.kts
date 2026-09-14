5b3a6c13b37045a08b421a45e4d326aee937be50
Safe Deals <shaikhnaeem2444-a11y@users.noreply.github.com>
2026-09-14 22:37:29 +0530

    Fix Safe Deals package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.safe_deals_build"
+    namespace = "com.safedeals.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.safe_deals_build"
+        applicationId = "com.safedeals.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
